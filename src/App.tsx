e79fc78dff7d2987fbf5047c509fd6e7f04e035e
Darion Williams <DarionJWilliams@Gmail.com>
2021-08-24 20:01:47 -0700

    hardcoded class list, began passing as prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,30 @@ import React, { useState } from 'react'
 import NavBar from './components/NavBar'
 import './App.css'
 
+interface archetypes {
+  warrior: string[];
+  martialArtist: string[];
+  gunner: string[];
+  mage: string[];
+  assassin: string[];
+}
+
+type archetypesStatic = Readonly<archetypes>
+
+let classes: archetypesStatic = {
+  warrior: ['Berserker', 'Paladin', 'Gunlancer'],
+  martialArtist: ['Striker', 'Wardancer', 'Scrapper', 'Soulfist'],
+  gunner: ['Gunslinger', 'Artillerist', 'Deadeye', 'Sharpshooter'],
+  mage: ['Bard', 'Summoner'],
+  assassin: ['Shadowhunter', 'Deathblade']
+}
+
 const App = (): JSX.Element => {
-  const [count, setCount] = useState(0)
+  let classes: string[] = []
 
   return (
     <div className="App">
-      <NavBar />
+      <NavBar classes={classes} />
     </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { AppBar } from '@material-ui/core'
 import { Toolbar } from '@material-ui/core'
 import { IconButton } from '@material-ui/core'
 import { Typography } from '@material-ui/core'
 import { Button } from '@material-ui/core'
-import { Menu } from '@material-ui/icons'
+import { Menu } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 
 type AppProps = {
-
+  classes: string[];
 }
 
 const NavBar = (props: AppProps): JSX.Element => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl((e.target as any).index);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="menu">
-            <Menu />
+            <MenuIcon />
           </IconButton>
-          <Typography variant="h4">
+          <Typography variant="h4" id="menu-title">
             trision.gg
           </Typography>
           <Button color="inherit" aria-label="new-build">New Build</Button>
