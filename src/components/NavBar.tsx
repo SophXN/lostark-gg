92953881e106e130e20974abd7d7b73a54e5ebc5
Darion Williams <DarionJWilliams@Gmail.com>
2021-08-24 18:02:22 -0700

    MaterialUI installed, inserted toolbar/navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { AppBar } from '@material-ui/core'
+import { Toolbar } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+import { Button } from '@material-ui/core'
+import { Menu } from '@material-ui/icons'
 
-const NavBar = (): JSX.Element => {
+type AppProps = {
 
+}
+
+const NavBar = (props: AppProps): JSX.Element => {
 
   return (
     <>
-      <p>Hi, I'm a navbar</p>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <Menu />
+          </IconButton>
+          <Typography variant="h4">
+            trision.gg
+          </Typography>
+          <Button color="inherit" aria-label="new-build">New Build</Button>
+        </Toolbar>
+      </AppBar>
     </>
   )
 }
